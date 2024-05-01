58da4e93ec110d57b3aa438dfca3efe4f7b90b19
iggonicollas <iggonicollas@gmail.com>
2024-05-01 11:56:32 -0300

    iniciando estudos de metodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+        
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        System.out.println("volume: " + smartTv.volume);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite canal: ");
+        int novoCanal = scanner.nextInt();
+        smartTv.canal = novoCanal;
+
+        System.out.println("Canal: " + smartTv.canal);
+
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,7 +1,14 @@
 public class TiposVariaveis {
    
-    public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500;
+    public static void main(String[] args) {
+        int a,b;
+        a=5;
+        b=6;
+
+        String resultado = a==b ?"cahorro" : "gato";
+        boolean booleanResultado = Boolean.parseBoolean(resultado);
+
+        System.out.println(booleanResultado);
 
     }
 }
